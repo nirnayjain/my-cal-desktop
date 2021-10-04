764db3c601ddd29b13a797ed0cf7667073e36945
Nirnay Jain <nirujainofficial@gmail.com>
2021-10-05 00:49:10 +0530

    input fixed

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ function App() {
 
   return (
     <div className="App" style={{display:'flex', gap:5}}>
-    <input type="text" value={sum.firstValue} onChange={(e)=>setSum({...sum,firstValue:Number(e.target.value)})} />
+    <input type="number" value={sum.firstValue} onChange={(e)=>setSum({...sum,firstValue:Number(e.target.value)})} />
      +
-    <input type="text" value={sum.secondValue} onChange={(e)=>setSum({...sum,secondValue:Number(e.target.value)})} />
+    <input type="number" value={sum.secondValue} onChange={(e)=>setSum({...sum,secondValue:Number(e.target.value)})} />
      =
      {sum.firstValue!=null &&sum.secondValue!=null?
      <>{sum.firstValue+sum.secondValue}</>
